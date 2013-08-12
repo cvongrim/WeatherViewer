9c43a93430a8514a35f4497ff706396fc5d39fc1
Colin <colinvong@gmail.com>
2013-08-12 10:57:30 -0400

    Initial Commit

---
+++ src/com/colinv/WeatherViewer/WeatherViewerActivity.java
@@ -0,0 +1,15 @@
+package com.colinv.WeatherViewer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeatherViewerActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
